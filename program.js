a33c8281255cdf389f3dc0c4aefbecf69b9f8e28
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-05-07 21:56:04 +0300

    Modify program.js

--- program.js
+++ program.js
@@ -3,23 +3,23 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    //if (input !== null) {
+    if (input !== null) {
       var instruction = input.toString().trim();
-      switch(instruction) {
-        case '':
-          process.stdout.write('You did not write anything! Try again\n');
-          process.exit();
-          break;
-        case '/exit':
-          process.stdout.write('Quitting app!\n');
-          process.exit();
-          break;
-        case "/env":
-          console.log("Language: " + process.env.LANG);
-          console.log("Node version: " + process.versions.node);
-          break;
-        default:
-          process.stderr.write('Wrong instruction!\n');
-      }
-    //}
+    }
+    switch(instruction) {
+      case '':
+        process.stdout.write('You did not write anything! Try again\n');
+        process.exit();
+        break;
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case "/env":
+        console.log("Language: " + process.env.LANG);
+        console.log("Node version: " + process.versions.node);
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    }
 });
