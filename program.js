1456ea3cd0f25156d14b597b4c6f0ae78ef1dcba
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-03-06 16:27:26 +0200

    Modify program.js

--- program.js
+++ program.js
@@ -1,26 +1,25 @@
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-    var input = process.stdin.read();
-    if (input !== null) {
-      var instruction = input.toString().trim();
-      return;
-    }
-    switch(instruction) {
-      case '':
-        process.stdout.write('You did not write anything! Try again\n');
-        process.exit();
-        break;
-      case '/exit':
-        process.stdout.write('Quitting app!\n');
-        process.exit();
-        break;
-      case "/env":
-        console.log("Language: " + process.env.LANG);
-        console.log("Node version: " + process.versions.node);
-        break;
-      default:
-        process.stderr.write('Wrong instruction!\n');
-    }
+process.stdin.on("readable", function() {
+  // .read() method to read what user type in input
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.toString().trim();
+  }
+  switch (instruction) {
+    case "":
+      process.stdout.write("You did not write anything! Try again\n");
+      process.exit();
+      break;
+    case "/exit":
+      process.stdout.write("Quitting app!\n");
+      process.exit();
+      break;
+    case "/env":
+      process.stdout.write("\nLanguage: " + process.env.LANG);
+      process.stdout.write("\nNode version: " + process.versions.node);
+      break;
+    default:
+      process.stderr.write("Wrong instruction!\n");
+  }
 });
